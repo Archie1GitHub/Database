80f1a5f24224c6a43637dfb05c5fa1bbe3c6236e
Archie1GitHub <114452146+Archie1GitHub@users.noreply.github.com>
2022-10-05 09:15:57 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Index</title>
+</head>
+<body>
+    <h1><a href = "table.php">Data Siswa</a><h1>
+    <h1><a href = "form.php">Form</a><h1>
+</body>
+</html>
